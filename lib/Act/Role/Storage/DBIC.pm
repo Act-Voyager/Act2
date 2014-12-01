0df16d7963ba216c6b7f4c362a0dcbbf0b2e2716
Philippe Bruhat (BooK) <book@cpan.org>
2014-12-01 18:11:46 +0100

    refactor Act::Role::Storage::DBIC
    - implements Act::Interface::Act (it is composed into the Act object)
    - implements Act::Interface::Storage
    - requires config
    - _translate_query translates queries sent to search_raw (against entities)
      into queries against the schema (almost a noop for now)

--- lib/Act/Role/Storage/DBIC.pm
+++ lib/Act/Role/Storage/DBIC.pm
@@ -1,33 +1,47 @@
 package Act::Role::Storage::DBIC;
 
 use Act::Schema;
+use Module::Runtime qw( require_module );
 use namespace::clean;
 
 use Moo::Role;
 
-has _schema => ( is => 'lazy' );
+requires 'config';
 
-has _entity_to_source => (
-    is      => 'lazy',
-    default => sub { {} },
+with qw(
+    Act::Interface::Act
+    Act::Interface::Storage
 );
 
+has _schema => ( is => 'lazy' );
+
 sub _build__schema {
-    Act::Schema->connect( @{ shift->config }{qw( uri user pass )} );
+    Act::Schema->connect(
+        @{ shift->config->{endpoint} }{qw( uri user pass )} );
 }
 
-sub _search_rs {
-    my ( $self, $entity_name, @args ) = @_;
-    my $source_name = $self->_entity_to_source->{$entity_name};
-    $self->_schema->resultset($source_name)->search_rs(@args);
+my $entity_to_source = {
+     Person => 'User',
+};
+
+sub _translate_query {
+    my ( $self, $entity_type, $query ) = @_;
+    return ( $entity_to_source->{$entity_type}, $query, {} );
 }
 
 sub search_raw {
-    my ( $self, $entity_name, @args ) = @_;
-    return $self->_search_rs( $entity_name, @args )->search(
-        {},
-        { result_class => "Act::Transformer::$entity_name"->new( $self->act ) }
-    )->all;
+    my ( $self, $entity_type, $query ) = @_;
+
+    my ( $source_name, $dbic_query, $dbic_opts )
+        = $self->_translate_query( $entity_type, $query );
+
+    my $transformer = "Act::Transformer::$entity_type";
+    require_module($transformer);
+
+    return $self->_schema->resultset($source_name)
+        ->search_rs( $dbic_query, $dbic_opts )
+        ->search( {}, { result_class => $transformer->new( act => $self ) } )
+        ->all;
 }
 
 1;
